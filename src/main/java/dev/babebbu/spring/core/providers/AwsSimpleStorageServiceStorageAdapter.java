aaf55ed6835a0b1fd329922e8918be3df34b04ec
Natthasak Vechprasit <9090871+babebbu@users.noreply.github.com>
2022-05-27 12:14:48 +0700

    Storage Adapters

--- src/main/java/dev/babebbu/spring/core/providers/AwsSimpleStorageServiceStorageAdapter.java
+++ src/main/java/dev/babebbu/spring/core/providers/AwsSimpleStorageServiceStorageAdapter.java
@@ -2,10 +2,17 @@ package dev.babebbu.spring.core.providers;
 
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
+
 @Service
 public class AwsSimpleStorageServiceStorageAdapter implements StorageAdapter {
     @Override
     public String save(byte[] content) {
+        return null;
+    }
+
+    @Override
+    public String save(byte[] content, String extension) throws IOException {
         return null;
     }
 }
